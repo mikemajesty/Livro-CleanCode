65353f9b879ccb00db49a52ff6c446b6c82b467f
mikemajesty <mikemajesty2011@gmail.com>
2016-01-18 21:59:37 -0200

    Chapter 10 - Classes

---
+++ ExerciciosDoLivroCleanCode/Chapter 10/SuperDashboard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExerciciosDoLivroCleanCode.Chapter_9
+{
+    public interface SuperDashboard
+    {
+        string getCustomizerLanguagePath();
+        void setSystemConfigPath(String systemConfigPath);
+        String getSystemConfigDocument();
+        void setSystemConfigDocument(String systemConfigDocument);
+        bool getGuruState();
+        bool getNoviceState();
+        bool getOpenSourceState();
+        void showObject(object MetaObject);
+        void showProgress(String s);
+    }
+}
---
+++ ExerciciosDoLivroCleanCode/Chapter 10/Version.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosDoLivroCleanCode.Chapter_9
+{
+    public interface Version
+    {
+
+             int getMajorVersionNumber();
+             int getMinorVersionNumber();
+             int getBuildNumber();
+    }
+}
